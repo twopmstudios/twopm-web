2a027aef1e3c5d241fca98bef91b9f4ebee8228b
Ben Follington <5009316+bfollington@users.noreply.github.com>
2020-03-25 16:33:15 +1000

    Test un-minified css

--- postcss.config.js
+++ postcss.config.js
@@ -4,8 +4,8 @@ module.exports = {
     require('postcss-nested'),
     require('postcss-simple-vars'),
     require('autoprefixer'),
-    require('cssnano')({
-      preset: 'default',
-    }),
+    // require('cssnano')({
+    //   preset: 'default',
+    // }),
   ]
 }
